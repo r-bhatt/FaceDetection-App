eb0007c41dfe4e75c7e30f078f115fb8001a1ca8
r-bhatt <you@example.com>
2020-02-11 12:49:07 +0530

    Updates

---
+++ precache-manifest.6d25e70ad790fc6f8dd7ccebff188302.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218ca6e9a6fbb0b8cff29b332a3e3636",
+    "url": "/FaceDetection-App/index.html"
+  },
+  {
+    "revision": "5db4d25c739934c91731",
+    "url": "/FaceDetection-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ae82d51f728fb205d936",
+    "url": "/FaceDetection-App/static/css/main.2e1e624f.chunk.css"
+  },
+  {
+    "revision": "5db4d25c739934c91731",
+    "url": "/FaceDetection-App/static/js/2.e5417cb8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FaceDetection-App/static/js/2.e5417cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae82d51f728fb205d936",
+    "url": "/FaceDetection-App/static/js/main.cfc72c25.chunk.js"
+  },
+  {
+    "revision": "2bc917f05b934c2136fa",
+    "url": "/FaceDetection-App/static/js/runtime-main.f5699702.js"
+  }
+]);
